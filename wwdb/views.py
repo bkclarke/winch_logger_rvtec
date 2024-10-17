b0b9f07773169047576f172fed8eba6852d1f12d
Bonny Clarke <bonny.k.clarke@gmail.com>
2024-10-17 20:57:37 +0000

    'wtf'

--- wwdb/views.py
+++ wwdb/views.py
@@ -44,17 +44,18 @@ def get_data_from_external_db(start_date, end_date, winch):
 
         winch=Winch.objects.get(id=winch)
 
-        start_date_str = start_date.strftime('%Y-%m-%d %H:%M:%S')
-        print(start_date_str)
-        end_date_str = end_date.strftime('%Y-%m-%d %H:%M:%S')
-        print(end_date_str)
-        print(winch)
+        start_date_plus_10min = start_date + timedelta(minutes=10)
+        end_date_minus_10min = end_date - timedelta(minutes=10)
+
+        start_date_str = start_date_plus_10min.strftime('%Y-%m-%d %H:%M:%S')
+        end_date_str = end_date_minus_10min.strftime('%Y-%m-%d %H:%M:%S')
+
+
         query = f"""
             SELECT date_time, tension_load_cell, payout
             FROM {winch}
             WHERE date_time BETWEEN '{start_date_str}' AND '{end_date_str}'
         """
-        print(query)
 
         cursor = conn.cursor()
         cursor.execute(query)
